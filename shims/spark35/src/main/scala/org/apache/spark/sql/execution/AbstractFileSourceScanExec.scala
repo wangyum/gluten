3fc643352648e4b4d56346e284af2042a13c1e6a
yumwang@ebay.com <yumwang@ebay.com>
2025-05-29 00:28:17 +0800

    fix

--- shims/spark35/src/main/scala/org/apache/spark/sql/execution/AbstractFileSourceScanExec.scala
+++ shims/spark35/src/main/scala/org/apache/spark/sql/execution/AbstractFileSourceScanExec.scala
@@ -16,8 +16,6 @@
  */
 package org.apache.spark.sql.execution
 
-import org.apache.gluten.execution.PartitionedFileUtilShim
-
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.{FileSourceOptions, InternalRow, TableIdentifier}
 import org.apache.spark.sql.catalyst.catalog.BucketSpec
