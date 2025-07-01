388528b145793abdde1ed533fe93acd7cd601a20
Yuming Wang <yumwang@ebay.com>
2025-07-01 22:04:41 +0800

    Build with eBay internal Spark

--- gluten-substrait/src/main/scala/org/apache/gluten/execution/BatchScanExecTransformer.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/execution/BatchScanExecTransformer.scala
@@ -29,6 +29,7 @@ import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.QueryPlan
 import org.apache.spark.sql.catalyst.util.truncatedString
 import org.apache.spark.sql.connector.catalog.Table
+import org.apache.spark.sql.connector.catalog.functions.Reducer
 import org.apache.spark.sql.connector.read.{InputPartition, Scan}
 import org.apache.spark.sql.execution.datasources.v2.{BatchScanExecShim, FileScan}
 import org.apache.spark.sql.execution.metric.SQLMetric
@@ -42,7 +43,9 @@ case class BatchScanExecTransformer(
     override val keyGroupedPartitioning: Option[Seq[Expression]] = None,
     override val ordering: Option[Seq[SortOrder]] = None,
     @transient override val table: Table,
+    override val joinKeyPositions: Option[Seq[Int]] = None,
     override val commonPartitionValues: Option[Seq[(InternalRow, Int)]] = None,
+    override val reducers: Option[Seq[Option[Reducer[_, _]]]] = None,
     override val applyPartialClustering: Boolean = false,
     override val replicatePartitions: Boolean = false)
   extends BatchScanExecTransformerBase(
@@ -52,9 +55,12 @@ case class BatchScanExecTransformer(
     keyGroupedPartitioning,
     ordering,
     table,
+    joinKeyPositions,
     commonPartitionValues,
+    reducers,
     applyPartialClustering,
-    replicatePartitions) {
+    replicatePartitions
+  ) {
 
   protected[this] def supportsBatchScan(scan: Scan): Boolean = {
     scan.isInstanceOf[FileScan]
@@ -77,7 +83,9 @@ abstract class BatchScanExecTransformerBase(
     override val keyGroupedPartitioning: Option[Seq[Expression]] = None,
     override val ordering: Option[Seq[SortOrder]] = None,
     @transient override val table: Table,
+    override val joinKeyPositions: Option[Seq[Int]] = None,
     override val commonPartitionValues: Option[Seq[(InternalRow, Int)]] = None,
+    override val reducers: Option[Seq[Option[Reducer[_, _]]]] = None,
     override val applyPartialClustering: Boolean = false,
     override val replicatePartitions: Boolean = false)
   extends BatchScanExecShim(
@@ -87,9 +95,12 @@ abstract class BatchScanExecTransformerBase(
     keyGroupedPartitioning,
     ordering,
     table,
+    joinKeyPositions,
     commonPartitionValues,
+    reducers,
     applyPartialClustering,
-    replicatePartitions)
+    replicatePartitions
+  )
   with BasicScanExecTransformer {
 
   // Note: "metrics" is made transient to avoid sending driver-side metrics to tasks.
