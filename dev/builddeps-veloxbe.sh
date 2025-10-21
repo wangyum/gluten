8e915ed0ebee74bd6c9917a773af31374ce5db1f
Yuming Wang <yumwang@ebay.com>
2025-10-21 21:03:27 +0800

    Add uv install tools

--- dev/builddeps-veloxbe.sh
+++ dev/builddeps-veloxbe.sh
@@ -272,8 +272,8 @@ function setup_dependencies {
 
   pwd
   file='../build/velox_ep/scripts/setup-common.sh'
-  sed -i '410d' "$file"
-  sed -i '409s|$| sh build-tools/install.sh|' "$file"
+  sed -i '429d' "$file"
+  sed -i '428s|$| sh build-tools/install.sh|' "$file"
   cat "$file"
 
   source $GLUTEN_DIR/dev/build-helper-functions.sh
