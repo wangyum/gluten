0d86fd784ac10815171436a275ee0f28a0ed096d
Yuming Wang <yumwang@ebay.com>
2025-10-27 16:30:22 +0800

    fix

--- gluten-substrait/src/main/scala/org/apache/gluten/utils/PartitionsUtil.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/utils/PartitionsUtil.scala
@@ -98,6 +98,8 @@ case class PartitionsUtil(
         partition =>
           SparkShimLoader.getSparkShims.getFileStatus(partition).flatMap {
             file =>
+              // scalastyle:off
+              println(s"===${file}===")
               // getPath() is very expensive so we only want to call it once in this block:
               val filePath = file._1.getPath
               if (shouldProcess(filePath)) {
