7f69b77bc2976d77d56554df52574599813a5206
Yuming Wang <yumwang@ebay.com>
2026-03-03 14:12:25 +0800

    Integration with internal Spark version

--- backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteForHiveSuite.scala
+++ backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteForHiveSuite.scala
@@ -274,7 +274,9 @@ class VeloxParquetWriteForHiveSuite extends GlutenQueryTest with SQLTestUtils wi
     if (isSparkVersionGE("3.4")) {
       Seq("true", "false").foreach {
         enableConvertMetastore =>
-          withSQLConf("spark.sql.hive.convertMetastoreParquet" -> enableConvertMetastore) {
+          withSQLConf(
+            SQLConf.HIVE_BUCKET_WRITE_COMPATIBLE.key -> "true",
+            "spark.sql.hive.convertMetastoreParquet" -> enableConvertMetastore) {
             val source = "hive_source_table"
             val target = "hive_bucketed_table"
             withTable(source, target) {
@@ -314,7 +316,9 @@ class VeloxParquetWriteForHiveSuite extends GlutenQueryTest with SQLTestUtils wi
     if (isSparkVersionGE("3.4")) {
       Seq("true", "false").foreach {
         enableConvertMetastore =>
-          withSQLConf("spark.sql.hive.convertMetastoreParquet" -> enableConvertMetastore) {
+          withSQLConf(
+            SQLConf.HIVE_BUCKET_WRITE_COMPATIBLE.key -> "true",
+            "spark.sql.hive.convertMetastoreParquet" -> enableConvertMetastore) {
             val source = "hive_source_table"
             val target = "hive_bucketed_table"
             withTable(source, target) {
@@ -346,7 +350,9 @@ class VeloxParquetWriteForHiveSuite extends GlutenQueryTest with SQLTestUtils wi
     if (isSparkVersionGE("3.4")) {
       Seq("true", "false").foreach {
         enableConvertMetastore =>
-          withSQLConf("spark.sql.hive.convertMetastoreParquet" -> enableConvertMetastore) {
+          withSQLConf(
+            SQLConf.HIVE_BUCKET_WRITE_COMPATIBLE.key -> "true",
+            "spark.sql.hive.convertMetastoreParquet" -> enableConvertMetastore) {
             val source = "hive_source_table"
             val target = "hive_bucketed_table"
             withTable(source, target) {
