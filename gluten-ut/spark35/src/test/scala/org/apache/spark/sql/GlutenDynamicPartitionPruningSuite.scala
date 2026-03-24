5b49d302de3e3f663cd9f02b41322d6c87a7744f
Yuming Wang <yumwang@ebay.com>
2026-03-24 14:05:25 +0800

    Integration with internal Spark version

--- gluten-ut/spark35/src/test/scala/org/apache/spark/sql/GlutenDynamicPartitionPruningSuite.scala
+++ gluten-ut/spark35/src/test/scala/org/apache/spark/sql/GlutenDynamicPartitionPruningSuite.scala
@@ -18,6 +18,7 @@ package org.apache.spark.sql
 
 import org.apache.gluten.config.GlutenConfig
 import org.apache.gluten.execution.{BatchScanExecTransformer, FileSourceScanExecTransformer, FilterExecTransformerBase}
+import org.apache.gluten.expression.VeloxBloomFilterMightContain
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.catalyst.expressions.{DynamicPruningExpression, Expression}
@@ -71,7 +72,7 @@ abstract class GlutenDynamicPartitionPruningSuiteBase
        """.stripMargin)
 
       val found = df.queryExecution.executedPlan.find {
-        case _ @BroadcastHashJoinExec(_, _, _: ExistenceJoin, _, _, _, _, _) => true
+        case _ @BroadcastHashJoinExec(_, _, _: ExistenceJoin, _, _, _, _, _, _) => true
         case _ => false
       }
 
@@ -235,12 +236,13 @@ abstract class GlutenDynamicPartitionPruningSuiteBase
     val plan = df.queryExecution.executedPlan
     val dpExprs = collectDynamicPruningExpressions(plan)
     val hasSubquery = dpExprs.exists {
-      case InSubqueryExec(_, _: SubqueryExec, _, _, _, _) => true
+      case InSubqueryExec(_, _: SubqueryExec, _, _, _, _, _) => true
+      case _: VeloxBloomFilterMightContain => true
       case _ => false
     }
     val subqueryBroadcast = dpExprs.collect {
-      case InSubqueryExec(_, b: SubqueryBroadcastExec, _, _, _, _) => b
-      case InSubqueryExec(_, b: ColumnarSubqueryBroadcastExec, _, _, _, _) => b
+      case InSubqueryExec(_, b: SubqueryBroadcastExec, _, _, _, _, _) => b
+      case InSubqueryExec(_, b: ColumnarSubqueryBroadcastExec, _, _, _, _, _) => b
     }
 
     val hasFilter = if (withSubquery) "Should" else "Shouldn't"
@@ -293,9 +295,9 @@ abstract class GlutenDynamicPartitionPruningSuiteBase
     df.collect()
 
     val buf = collectDynamicPruningExpressions(df.queryExecution.executedPlan).collect {
-      case InSubqueryExec(_, b: SubqueryBroadcastExec, _, _, _, _) =>
+      case InSubqueryExec(_, b: SubqueryBroadcastExec, _, _, _, _, _) =>
         b.index
-      case InSubqueryExec(_, b: ColumnarSubqueryBroadcastExec, _, _, _, _) =>
+      case InSubqueryExec(_, b: ColumnarSubqueryBroadcastExec, _, _, _, _, _) =>
         b.indices
     }
     assert(buf.distinct.size == n)
